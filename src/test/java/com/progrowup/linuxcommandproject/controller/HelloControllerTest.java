fea493abbbb89ea7499d85bd08b372b457686b91
mikekang47 <gyeong0308@gmail.com>
2022-05-30 20:59:18 +0900

    Add: create HelloControllerTest

---
+++ src/test/java/com/progrowup/linuxcommandproject/controller/HelloControllerTest.java
@@ -0,0 +1,17 @@
+package com.progrowup.linuxcommandproject.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloControllerTest {
+    HelloController helloController = new HelloController();
+
+    @Test
+    void sayHello() {
+        assertThat(helloController.sayHello()).isEqualTo("Hello World!");
+    }
+}
