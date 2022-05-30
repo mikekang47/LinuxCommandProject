6f7ebdee6463e5d57cc9253033346c0e8a3a9145
mikekang47 <gyeong0308@gmail.com>
2022-05-30 20:34:07 +0900

    Set environment

---
+++ src/main/java/com/progrowup/linuxcommandproject/LinuxCommandProjectApplication.java
@@ -0,0 +1,13 @@
+package com.progrowup.linuxcommandproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxCommandProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxCommandProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/progrowup/linuxcommandproject/LinuxCommandProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progrowup.linuxcommandproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxCommandProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
